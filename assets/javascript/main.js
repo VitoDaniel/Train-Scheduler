9cef3d84a7b026d1b766d3fe041bf6936c0d2268
Vito Daniel <vitodaniel93@gmail.com>
2018-07-25 22:59:26 -0400

    pseudocoding

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,11 @@
-alert("Hello there");
+// add CDN for Firebase html script
+// firebase configuration
+// initialize the firebase
+// capture the users inputs 
+    // clear the form for the next train.
+    // trim() the inputs , create an object of the data
+//create new data in database
+// get back data from database
+// use momentjs to calculate time of the next train arrival
+// use momentjs to calculate minutes away
+// create html elements  and append it to the page
